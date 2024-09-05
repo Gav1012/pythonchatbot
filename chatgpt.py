97d2d74b9bf786b9de7066dac97e7a6a7c180c2c
Gav1012 <gavin.poley@gmail.com>
2024-09-04 23:59:43 -0700

    imported openai setup, will need to make changes

--- chatgpt.py
+++ chatgpt.py
@@ -1,4 +1,19 @@
 import os
-from openai import OpenAI
+import openai
+from dotenv import load_dotenv
 
-print("hello from chatgpt.py file")
+load_dotenv()
+
+api_key = os.getenv("CHATGPT_API_KEY")
+
+# print("API KEY: ", api_key)
+
+ai_response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are Cayde-6 from the game series Destiny 2"},
+        {"role": "user", "content": "What is your favorite food?"}
+    ]
+)
+
+print(ai_response["choices"][0]["message"]["content"])
--- openai.py
+++
@@ -1,1 +0,0 @@
-print("this is where the openai code will go")
