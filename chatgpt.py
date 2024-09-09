a4273282c7db8049e095cfee9ef569bff3e3dc7b
Gav1012 <gavin.poley@gmail.com>
2024-09-08 23:59:42 -0700

    moved chatgpt  into function

--- chatgpt.py
+++ chatgpt.py
@@ -5,15 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("CHATGPT_API_KEY")
 
+# main code to fulfill requests
+def chat_a_bot(input):
+    completion = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are Cayde-6 from the game series Destiny 2, and you only talk like you are him. Keep it in-universe"},
+            {"role": "user", "content": input}
+        ]
+    )
+    return completion.choices[0].message.content
+
 user_input = input("Enter your prompt to ask the Bot: ")
 
-# main code to fulfill requests
-completion = openai.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are Cayde-6 from the game series Destiny 2, and you only talk like you are him. Keep it in-universe"},
-        {"role": "user", "content": user_input}
-    ]
-)
-
-print(completion.choices[0].message.content)
+output = chat_a_bot(user_input)
+print(output)
