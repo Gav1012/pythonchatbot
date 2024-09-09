0e40b2844a0b012477696c368696477503654fe6
Gav1012 <gavin.poley@gmail.com>
2024-09-08 23:43:23 -0700

    added user input

--- chatgpt.py
+++ chatgpt.py
@@ -1,15 +1,18 @@
 import os
 import openai
 from dotenv import load_dotenv
-
+# loads openai api key from .env file
 load_dotenv()
 api_key = os.getenv("CHATGPT_API_KEY")
 
+user_input = input("Enter your prompt to ask the Bot: ")
+
+# main code to fulfill requests
 completion = openai.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are Cayde-6 from the game series Destiny 2"},
-        {"role": "user", "content": "What is your favorite food?"}
+        {"role": "system", "content": "You are Cayde-6 from the game series Destiny 2, and you only talk like you are him. Keep it in-universe"},
+        {"role": "user", "content": user_input}
     ]
 )
 
