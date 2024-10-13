65077031063cbe3d67a34c8f3942cd94ccbf10d6
Gav1012 <gavin.poley@gmail.com>
2024-10-12 23:35:59 -0700

    more stuff for microsoft azure

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,6 +1,15 @@
 import os
 import azure.cognitiveservices.speech as speechsdk
 
-azure_speechconfig = None
-azure_audioconfig = None
-azure_speechrecognizer = None
+# subscription_key = API KEY
+# service_region = REGION
+
+# azure_speechconfig = None
+# azure_audioconfig = None
+# azure_speechrecognizer = None
+
+azure_speechconfig = speech.sdk.SpeechConfig(subscription=subscription_key, region=region)
+
+synthesizer = speechsdk.SpeechSynthesizer(azure_audioconfig=azure_audioconfig)
+
+result = synthesizer.speak_text_async(text).get()
