f4eeb13982cd18d58aa4a2d9b4a2a1ab0542e7b6
Gav1012 <gavin.poley@gmail.com>
2024-11-16 23:33:00 -0800

    changed function to return only the text of the speech and fixed test code

--- speech_to_text.py
+++ speech_to_text.py
@@ -11,6 +11,7 @@ class AzureSpeechToText:
             self.azure_speechconfig = speechsdk.SpeechConfig(subscription=os.getenv("AZURE_SPEECH_KEY"), region=os.getenv("AZURE_REGIONKEY"))
         except TypeError:
             print("Error: Azure Speech Key or Region Key not found")
+    
     # function to grab voice from mic and put it into text
     def transcribe_from_mic(self):
         audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
@@ -18,14 +19,20 @@ class AzureSpeechToText:
 
         print("Hot Mic Mode...")
         # grabs from mc
-        result = speech_recognizer.recognize_once()
+        result = speech_recognizer.recognize_once_async()
         # parses the result from the mic with currently 3 options to return
         if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-            return result.text
+            return print("found recognized speech")
         elif result.reason == speechsdk.ResultReason.NoMatch:
-            return "No Match"
+            return print("no speech was recognized")
         elif result.reason == speechsdk.ResultReason.Canceled:
-            return "Speech Cancelled"
+            return print("speech recognition was cancelled")
+        
+        return result.text
 # example of speech being used in file
 if __name__ == '__main__':
     speechtotext_azure = AzureSpeechToText()
+
+    while True:
+        speech_result = speechtotext_azure.transcribe_from_mic()
+        print("speech result", speech_result)
